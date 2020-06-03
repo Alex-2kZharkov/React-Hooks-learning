1bada80a6ad367e240347ac62a8ad32696b01279
Alelsis-2kVampire <alex@MacBook-Pro.local>
2020-06-04 00:15:23 +0600

    Learning how to work with useState hook and arrays

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UseStateArray from './UseStateArray';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <UseStateArray />
+    </div>
+  );
 }
 
 export default App;
