a36d2b49f2a35ba1d1ce0c6a8d654ce4a31ec78b
Alex-2kZharkov <Alexander.06.zharkov@gmail.com>
2020-06-05 16:07:10 +0600

    Simple data fetching with axios and https://jsonplaceholder.typicode.com

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 import UseEffectOnce from './UseEffectOnce';
 import MouseContainer from './MouseContainer';
+import DataFetching from './DataFetching';
 
 function App() {
   return (
     <div className='App'>
-      <MouseContainer />
+    <DataFetching />
     </div>
   );
 }
