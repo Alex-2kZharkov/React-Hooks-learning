1e14382b0ef481ef298488fbfe5d3f4ce71139c9
Alex-2kZharkov <Alexander.06.zharkov@gmail.com>
2020-06-05 16:49:00 +0600

    Added button to limit rerender and request with every change in input field

--- src/DataFetching.js
+++ src/DataFetching.js
@@ -4,10 +4,11 @@ import axios from 'axios';
 function DataFetching() {
   const [post, setPost] = useState({});
   const [id, setId] = useState(1);
+  const [idFromButton, setIdFromButton] = useState(1);
 
   useEffect(() => {
     axios // axios for making http requests
-      .get(`https://jsonplaceholder.typicode.com/posts/${id}`) // get single post from request
+      .get(`https://jsonplaceholder.typicode.com/posts/${idFromButton}`) // get single post from request
       .then((response) => {
         console.log(response.data);
         setPost(response.data);
@@ -15,10 +16,13 @@ function DataFetching() {
       .catch((error) => {
         console.log(error);
       });
-  }, [id]); // depend on id
+  }, [idFromButton]); // depend on id
   return (
     <div>
       <input value={id} onChange={(e) => setId(e.target.value)} />
+      <button type='button' onClick={() => setIdFromButton(id)}>
+        Get specific single post
+      </button>
       <h2>Id</h2> <span>{post.id}</span>
       <h2>Title</h2> <span>{post.title}</span>
       <h2>Body</h2> <span>{post.body}</span>
